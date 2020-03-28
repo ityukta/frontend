9dbcc4a040775ee9eb3124c847a38cea1d9ef37a
adwaith200 <61187848+adwaith200@users.noreply.github.com>
2020-03-28 17:35:46 +0530

    added ia page

--- js/openassess.js
+++ js/openassess.js
@@ -1,23 +1,18 @@
 let main=()=>{
-    let btn;
-    document.querySelector('.teacherli').addEventListener('click',(e)=>{
-        e.preventDefault();
-        document.querySelector('.sub').classList.toggle('show');
-    });
-    document.querySelector('.studentli').addEventListener('click',(e)=>{
-        e.preventDefault();
-        document.querySelector('.studentlist').classList.toggle('show');
-    });
+    let assess;
+  
     document.querySelector('.close-btn').addEventListener('click',(e)=>{
         e.target.parentNode.parentNode.parentNode.classList.add('modal__dispnone');
     });
+    /*
     document.querySelector('.fa-plus').addEventListener('click',(e)=>{
         console.log( document.querySelector('.modal'));
        document.querySelector('.modal').classList.toggle('modal__dispnone'); 
-    });
-        document.querySelectorAll('.assignclass__add__pic').forEach(ele=>{
-        ele.addEventListener('click',(e)=>{
-        document.querySelector('.modal').classList.toggle('modal__dispnone'); 
+    });*/
+    assess=document.querySelectorAll('.asessment');
+    assess.forEach(ele=>{
+        ele.addEventListener('click',(e)=>{    
+            document.querySelector('.modal').classList.toggle('modal__dispnone'); 
         })
     })
 }
